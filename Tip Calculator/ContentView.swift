9fdc9719e327da6320975e4dfee811581f54cad9
Paul Pan <panjunhong05@gmail.com>
2020-01-14 15:04:16 -0800

    Initial Commit

---
+++ Tip Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by Paul Pan on 20/14/1.
+//  Copyright © 2020 Paul Pan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
