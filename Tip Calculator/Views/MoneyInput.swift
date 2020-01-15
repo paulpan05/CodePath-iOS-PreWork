cb154b4f936345430f216fd57faa5e465314d656
Paul Pan <panjunhong05@gmail.com>
2020-01-14 20:54:11 -0800

    Revised initial code

--- Tip Calculator/Views/MoneyInput.swift
+++ Tip Calculator/Views/MoneyInput.swift
@@ -12,20 +12,61 @@ struct MoneyInput: View {
     @State private var amount: String = ""
     @State private var selectionIndex: Int = 0
     @State private var tipAmount = ["10%", "15%", "20%"]
+    private var calculatedColor: Color {
+        var currentColor = Color.green
+        currentColor = currentColor.opacity(0.4)
+        return currentColor
+    }
+    func calculateTip() -> Double {
+        var result = Double(amount) ?? 0
+        result = result * (0.1 + Double(selectionIndex) * 0.05)
+        return result
+    }
+    func calculateTotal() -> Double {
+        var result = Double(amount) ?? 0
+        result = result * (1.1 + Double(selectionIndex) * 0.05)
+        return result
+    }
     var body: some View {
         VStack {
-            Spacer()
-            TextField("$", text: $amount).keyboardType(.decimalPad).font(.system(size: 58)).multilineTextAlignment(.trailing)
-            Picker("Tip Amount", selection: $selectionIndex) {
-                ForEach(0 ..< tipAmount.count) {index in
-                    Text(self.tipAmount[index]).tag(index)
+            VStack{
+                Spacer()
+                TextField("$", text: $amount).keyboardType(.decimalPad).font(.system(size: 78)).multilineTextAlignment(.trailing)
+                Picker("Tip Amount", selection: $selectionIndex) {
+                    ForEach(0 ..< tipAmount.count) {index in
+                        Text(self.tipAmount[index]).tag(index)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                Spacer()
+            }
+            .padding(.all)
+            .background(calculatedColor)
+            VStack{
+                HStack {
+                    Text("+").font(.system(size: 30))
+                    Spacer()
+                    Text("\(String(format: "%.2f", calculateTip()))").font(.system(size: 30))
+                }
+                HStack {
+                    Spacer()
+                    Text("\(String(format: "%.2f", calculateTotal() * 4))").font(.system(size: 110))
+                }
+                HStack {
+                    Spacer()
+                    Text("\(String(format: "%.2f", calculateTotal() * 3))").font(.system(size: 90))
+                }
+                HStack {
+                    Spacer()
+                    Text("\(String(format: "%.2f", calculateTotal() * 2))").font(.system(size: 70))
+                }
+                HStack {
+                    Spacer()
+                    Text("\(String(format: "%.2f", calculateTotal()))").font(.system(size: 50))
                 }
             }
-            .pickerStyle(SegmentedPickerStyle())
-            //Text("\(String(format: "%.2f", Double(amount) ?? 0))")
-            Spacer()
+            .background(Color.green)
         }
-        .background(Color.green)
     }
 }
 
